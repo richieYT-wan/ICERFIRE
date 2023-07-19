d29a572eb8cb5c68c72cc1d70ecc12d89f92288c
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2023-07-19 14:54:49 +0200

    reformat savename

--- src/run_model.py
+++ src/run_model.py
@@ -14,9 +14,6 @@ if parent_dir not in sys.path:
 src_path = '/home/local/tools/src/ICERFIRE-1.0/src/'
 # Add the "src" directory to the Python module search path
 sys.path.append(src_path)
-print(sys.path)
-print(src_path)
-os.listdir(src_path)
 # print(module_path, sys.path)
 from train_eval import evaluate_trained_models
 from mutation_tools import pipeline_mutation_scores
@@ -46,17 +43,16 @@ def main():
     run_dt = get_datetime_string()
     run_id = get_random_id(6)
     run_tag = 'AddExpr' if args['add_expression'] else 'NoExpr'
-    basename = os.path.basename(args['infile']).split('.')[0]
-    print('basename', basename)
+    # basename = os.path.basename(args['infile']).split('.')[0]
+    # print('basename', basename)
     print('infile, pepxfile', args['infile'], args['pepxpath'])
-    run_name = f'{run_dt}_{basename}_{run_tag}_{run_id}'
+    run_name = f'{run_dt}_{run_tag}_{run_id}'
     outdir = os.path.join(args['outdir'], f'{run_name}/')
     mkdirs(outdir)
     jobid = str(args['jobid'])
     # Get the directory one level above the script
     parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/'
     # Load appropriate model
-    # TODO: TRAIN/ADD MODEL W/O EXPRESSION
     unpickle = pkl_load(f'{parent_dir}saved_models/ICERFIRE_Expr{args["add_expression"]}.pkl')
     models, kwargs, ics = unpickle['model'], unpickle['kwargs'], unpickle['ics']
     data = pd.read_csv(args['infile'], sep=' ')
@@ -80,30 +76,27 @@ def main():
     predictions, test_results = evaluate_trained_models(data, models, ics, encoding_kwargs=kwargs, test_mode=True,
                                                         n_jobs=8)
     # Saving results as CSV table
-    predictions.sort_values('Peptide', ascending=True).to_csv(f'{outdir}{run_name}_predictions.csv',
+    predictions.sort_values('Peptide', ascending=True).to_csv(f'{outdir}ICERFIRE_predictions.csv',
                                                               columns=cols_to_save, index=False)
     if test_results is not None:
         pd.DataFrame(test_results).rename(columns={k: v for k, v in zip(range(len(test_results.keys())),
                                                                         [f'fold_{x}' for x in
                                                                          range(1, len(test_results.keys()))])}) \
-            .to_csv(f'{outdir}{run_name}_metrics_per_fold.csv', index=False)
-    print(f'Results saved at {outdir}{run_name}_predictions.csv')
+            .to_csv(f'{outdir}ICERFIRE_metrics_per_fold.csv', index=False)
+    print(f'Results saved at {outdir}ICERFIRE_predictions.csv')
     # Cleaning input/temporary files and returning the final saved filename
     for f in os.listdir(args['outdir']):
         if f.endswith('.csv') or f.endswith('.txt'):
             os.remove(os.path.join(args['outdir'], f))
 
-    return predictions, f'{outdir}{run_name}_predictions.csv'
+    return predictions, run_name, jobid
 
 
 if __name__ == '__main__':
-    predictions, filename = main()
-    # TODO : MAKE THE STUPID HYPERLINK TO DOWNLOAD, AND MAYBE FIX THE OUTDIR SO IT'S NOT AS CONVOLUTED
-    # print('Click ' + '<a href="https://services.healthtech.dtu.dk/services/NetTCR-2.0/tmp/' + args.jobid +
-    #       '/nettcr_predictions.csv" target="_blank">here</a>' + ' to download the results in .csv format.')
-
-    # print(pred_df, file=sys.stdout)
-    # print('Total time elapsed (without import): {:.1f} s '.format(time.time()-s_time))
+    predictions, run_name, jobid = main()
+    print('Click ' + '<a href="https://services.healthtech.dtu.dk/services/ICERFIRE-1.0/tmp/' \
+          + f'{jobid}/{run_name}/' \
+            'ICERFIRE_predictions.csv" target="_blank">here</a>' + ' to download the results in .csv format.')
 
     print("\n \nBelow is a table represention of binding predictions between T-Cell receptor and peptides. \n \n")
 
